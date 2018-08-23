0e67e2bf5ebd5f7f3e9a856bce0efb7c432cd17d
zhangpei <zhangpe0312@qq.com>
2018-08-23 23:01:03 +0800

    提交代码，删除链表

---
+++ src/main/java/org/nix/learn/link/ListNode.java
@@ -0,0 +1,16 @@
+package org.nix.learn.link;
+
+/**
+ * @author zhangpei
+ * @version 1.0
+ * @date 2018/8/23
+ */
+public class ListNode {
+
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}
