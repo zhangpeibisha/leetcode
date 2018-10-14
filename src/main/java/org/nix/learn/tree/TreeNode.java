3cb69c10f1617080f34b4ce137f8c99d2a09980c
zhangpei <zhangpe0312@qq.com>
2018-10-14 20:11:25 +0800

    通过数组生成二叉树

--- src/main/java/org/nix/learn/tree/TreeNode.java
+++ src/main/java/org/nix/learn/tree/TreeNode.java
@@ -3,23 +3,53 @@ package org.nix.learn.tree;
 /**
  * @author zhangpei
  * @version 1.0
- * @date 2018/8/18
+ * @date 2018/10/14
  */
-public class TreeNode {
-    int val;
-    TreeNode left;
-    TreeNode right;
+public class TreeNode<T> {
 
-    TreeNode(int x) {
-        val = x;
+    private TreeNode<T> left;
+
+    private TreeNode<T> right;
+
+    private T data;
+
+    public TreeNode() {
+    }
+
+    public TreeNode(T data) {
+        this.data = data;
+    }
+
+    public TreeNode<T> getLeft() {
+        return left;
+    }
+
+    public void setLeft(TreeNode<T> left) {
+        this.left = left;
+    }
+
+    public TreeNode<T> getRight() {
+        return right;
+    }
+
+    public void setRight(TreeNode<T> right) {
+        this.right = right;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
     }
 
     @Override
     public String toString() {
         return "TreeNode{" +
-                "val=" + val +
-                ", left=" + left +
+                "left=" + left +
                 ", right=" + right +
+                ", data=" + data +
                 '}';
     }
 }
